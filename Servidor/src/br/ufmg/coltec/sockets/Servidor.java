4a4e1674165c6b42f9fc86e42e13b279ae2d10f7
GustavinhoHenrique <ujhenrique@gmail.com>
2018-11-22 17:25:13 -0200

    Iniciando

---
+++ Servidor/src/br/ufmg/coltec/sockets/Servidor.java
@@ -0,0 +1,27 @@
+package br.ufmg.coltec.sockets;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class Servidor {
+
+    public static void main(String[] args) throws
+            IOException {
+
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
+
+        Scanner scanner = new Scanner (cliente.getInputStream());
+        while(scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+
+        scanner.close();
+        servidor.close();
+        cliente.close();
+    }
+}
